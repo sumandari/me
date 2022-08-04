d2b405a178b89473f87ba56ebf28a2dc689107fe
Sumandari <sumandari.ee@gmail.com>
2022-08-04 15:43:55 +0200

    create temporary landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="./qrcode.png" className="App-logo" alt="logo" />
+        <span className='App-header-text'><span className="App-halo">Hej!</span><img src="coffee.png" className='App-header-img' /></span>
       </header>
+      <footer className='App-footer'>
+        <small className='App-footer-text'>
+          This site is under development. Visit <a href="https://sumandari.github.com">older version</a>. <br />
+          Icon by <a href="https://freeicons.io/profile/722"> Fasil </a> on <a href="https://freeicons.io"> freeicons.io </a>
+        </small>
+      </footer>
     </div>
   );
 }
