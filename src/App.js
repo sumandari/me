8fe0e25a41059d6706f3e0eed9db0892d8a7a859
Sumandari <sumandari.ee@gmail.com>
2022-08-05 13:25:04 +0200

    fix image atribute and css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import coffee from './coffee.png';
+import qrcode from './qrcode.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./qrcode.png" className="App-logo" alt="logo" />
-        <span className='App-header-text'><span className="App-halo">Hej!</span><img src="coffee.png" className='App-header-img' /></span>
+        <img src={qrcode} className="App-logo" alt="logo" />
+        <span className='App-header-text'><span className="App-halo">Hej!</span><img src={coffee} className='App-header-img' alt="coffee"/></span>
       </header>
       <footer className='App-footer'>
         <small className='App-footer-text'>
-          This site is under development. Visit <a href="https://sumandari.github.com">older version</a>. <br />
-          Icon by <a href="https://freeicons.io/profile/722"> Fasil </a> on <a href="https://freeicons.io"> freeicons.io </a>
+          This site is under development. Visit <a href="https://sumandari.github.io">older version</a>. <br />
+          Built with ReactJs. Icon by <a href="https://freeicons.io/profile/722"> Fasil </a> on <a href="https://freeicons.io"> freeicons.io </a>
         </small>
       </footer>
     </div>
